53bac4e23add38d0dac13b71e9fb38e7fa868307
Morgane ROY <morgane.roy@alcuin.com>
2019-05-16 11:22:38 +0200

    Premier commit to master

---
+++ OutilRessources/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace OutilRessources
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileRessources = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectFile = new System.Windows.Forms.Button();
+            this.validImport = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileModele = new System.Windows.Forms.SaveFileDialog();
+            this.NameFile = new System.Windows.Forms.Label();
+            this.dataGridViewExcel = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExcel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FileRessources
+            // 
+            this.FileRessources.FileName = "FileRessources";
+            this.FileRessources.ReadOnlyChecked = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(760, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenue sur l\'outil d\'import de ressources.\r\nMerci de séléctionner votre fichie" +
+    "r et de cliquer sur le bouton importer.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Firebrick;
+            this.label2.Location = new System.Drawing.Point(24, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(760, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ATTENTION : Seuls les fichiers Excel (.xlsx ou .xls) sont acceptés";
+            // 
+            // selectFile
+            // 
+            this.selectFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.selectFile.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectFile.Location = new System.Drawing.Point(29, 112);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(1172, 53);
+            this.selectFile.TabIndex = 3;
+            this.selectFile.Text = "Sélectionner votre fichier d\'import";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
+            // 
+            // validImport
+            // 
+            this.validImport.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.validImport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.validImport.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validImport.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.validImport.Location = new System.Drawing.Point(29, 666);
+            this.validImport.Margin = new System.Windows.Forms.Padding(0);
+            this.validImport.Name = "validImport";
+            this.validImport.Size = new System.Drawing.Size(1172, 51);
+            this.validImport.TabIndex = 4;
+            this.validImport.TabStop = false;
+            this.validImport.Text = "Importer";
+            this.validImport.UseVisualStyleBackColor = false;
+            this.validImport.Click += new System.EventHandler(this.validImport_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aideToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1231, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aideToolStripMenuItem
+            // 
+            this.aideToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.downloadTemplateToolStripMenuItem});
+            this.aideToolStripMenuItem.Name = "aideToolStripMenuItem";
+            this.aideToolStripMenuItem.Size = new System.Drawing.Size(60, 29);
+            this.aideToolStripMenuItem.Text = "Aide";
+            // 
+            // downloadTemplateToolStripMenuItem
+            // 
+            this.downloadTemplateToolStripMenuItem.Name = "downloadTemplateToolStripMenuItem";
+            this.downloadTemplateToolStripMenuItem.Size = new System.Drawing.Size(418, 30);
+            this.downloadTemplateToolStripMenuItem.Text = "Télécharger le fichier de modèle d\'import";
+            this.downloadTemplateToolStripMenuItem.Click += new System.EventHandler(this.downloadTemplateToolStripMenuItem_Click);
+            // 
+            // saveFileModele
+            // 
+            this.saveFileModele.FileName = "ModeleRessources";
+            // 
+            // NameFile
+            // 
+            this.NameFile.AutoSize = true;
+            this.NameFile.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameFile.Location = new System.Drawing.Point(42, 307);
+            this.NameFile.Name = "NameFile";
+            this.NameFile.Size = new System.Drawing.Size(0, 23);
+            this.NameFile.TabIndex = 7;
+            // 
+            // dataGridViewExcel
+            // 
+            this.dataGridViewExcel.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewExcel.Location = new System.Drawing.Point(29, 225);
+            this.dataGridViewExcel.Name = "dataGridViewExcel";
+            this.dataGridViewExcel.RowTemplate.Height = 28;
+            this.dataGridViewExcel.Size = new System.Drawing.Size(1172, 420);
+            this.dataGridViewExcel.TabIndex = 8;
+            // 
+            // DashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 742);
+            this.Controls.Add(this.dataGridViewExcel);
+            this.Controls.Add(this.NameFile);
+            this.Controls.Add(this.validImport);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DashboardForm";
+            this.Text = "Outil d\'import de ressources";
+            this.Load += new System.EventHandler(this.DashboardForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExcel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog FileRessources;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.Button validImport;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem downloadTemplateToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileModele;
+        private System.Windows.Forms.Label NameFile;
+        private System.Windows.Forms.DataGridView dataGridViewExcel;
+    }
+}
+
---
+++ OutilRessources/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace OutilRessources
+{
+    public partial class DashboardForm : Form
+    {
+        System.Data.DataTable dtListeRes = new System.Data.DataTable();
+        OpenFileDialog fdlg = new OpenFileDialog();
+        string fname = "";
+
+        public DashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectFile_Click(object sender, EventArgs e)
+        {
+            
+            fdlg.Filter = "Excel Sheet(*.xlsx)|*.xlsx|All Files(*.*)|*.*";
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                fname = fdlg.FileName;
+            }
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Workbook xlWorkbook = xlApp.Workbooks.Open(fname);
+
+            //First sheet -> to table RES_ListeRes
+            Worksheet xlWorksheetListeRes = xlWorkbook.Sheets[1];
+            Range xlRangeListeRes = xlWorksheetListeRes.UsedRange;
+
+            int rowCount = xlRangeListeRes.Rows.Count;
+            int colCount = xlRangeListeRes.Columns.Count;
+            
+            dtListeRes.Columns.AddRange(new DataColumn[9] {
+                new DataColumn("intID_res", typeof(int)),
+                new DataColumn("charType_res", typeof(string)),
+                new DataColumn("charNom_res", typeof(string)),
+                new DataColumn("intParent_res", typeof(int)),
+                new DataColumn("charLstParents_res", typeof(string)),
+                new DataColumn("intFils_res", typeof(int)),
+                new DataColumn("intNiveau_res", typeof(int)),
+                new DataColumn("intOrdre_res", typeof(int)),
+                new DataColumn("intCommu_res", typeof(int))
+            });
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+                int intId_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 1].Value2.ToString());
+                string chartype_res = xlRangeListeRes.Cells[i, 2].Value2.ToString();
+                string charnom_res = xlRangeListeRes.Cells[i, 3].Value2.ToString();
+                int intparent_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 4].Value2.ToString());
+                string charLstParents_res = xlRangeListeRes.Cells[i, 5].Value2.ToString();
+                int intfils_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 6].Value2.ToString());
+                int intniveau_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 7].Value2.ToString());
+                int intordre_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 8].Value2.ToString());
+                int intcommu_res = Convert.ToInt32(xlRangeListeRes.Cells[i, 9].Value2.ToString());
+                dtListeRes.Rows.Add(intId_res, chartype_res, charnom_res, intparent_res, charLstParents_res, intfils_res, intniveau_res, intordre_res, intcommu_res);
+
+            }
+
+            NameFile.Text = "Fichier séléctionné : " + fdlg.SafeFileName;
+
+            for(int i = 1; i <= rowCount; i++)
+            {
+                
+                for(int j = 1; j <= colCount; j++)
+                {
+                    if (xlRangeListeRes.Cells[i, j] != null && xlRangeListeRes.Cells[i, j].Value2 != null)
+                    {
+                        dataGridViewExcel.Rows[i].Cells[j].Value = xlRangeListeRes.Cells[i, j].Value2.ToString();
+                    }
+                        
+                }
+            }
+
+            //cleanup  
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background  
+            Marshal.ReleaseComObject(xlRangeListeRes);
+            Marshal.ReleaseComObject(xlWorksheetListeRes);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+            
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+
+        private void downloadTemplateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = "ModeleRessources";
+            savefile.Filter = "Excel Sheet(*.xlsx)|*.xlsx|All Files(*.*)|*.*";
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                string sourcePath = System.Windows.Forms.Application.StartupPath;
+                File.Copy(sourcePath + "\\ModeleRessources.xlsx", savefile.FileName);
+            }
+        }
+
+        private void validImport_Click(object sender, EventArgs e)
+        {
+            if (dtListeRes.Rows.Count > 0)
+            {
+                string str = ConfigurationManager.ConnectionStrings["MaConnection"].ConnectionString;
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(str))
+                    {
+                        try
+                        {
+                            using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                            {
+                                sqlBulkCopy.DestinationTableName = "dbo.RES_ListeRes";
+                                sqlBulkCopy.ColumnMappings.Add("intID_res", "intID_res");
+                                sqlBulkCopy.ColumnMappings.Add("charType_res", "charType_res");
+                                sqlBulkCopy.ColumnMappings.Add("charNom_res", "charNom_res");
+                                sqlBulkCopy.ColumnMappings.Add("intParent_res", "intParent_res");
+                                sqlBulkCopy.ColumnMappings.Add("charLstParents_res", "charLstParents_res");
+                                sqlBulkCopy.ColumnMappings.Add("intFils_res", "intFils_res");
+                                sqlBulkCopy.ColumnMappings.Add("intNiveau_res", "intNiveau_res");
+                                sqlBulkCopy.ColumnMappings.Add("intOrdre_res", "intOrdre_res");
+                                sqlBulkCopy.ColumnMappings.Add("intCommu_res", "intCommu_res");
+                                con.Open();
+                                sqlBulkCopy.WriteToServer(dtListeRes);
+                                con.Close();
+                                MessageBox.Show("L'import a bien réussi :) ");
+                                NameFile.Text = "";
+                                fdlg = new OpenFileDialog();
+                                fname = "";
+
+                            }
+                        }
+                        catch(Exception exception)
+                        {
+                            MessageBox.Show("Erreur dans l'import : " + exception.Message);
+                        }
+                    }
+                }
+                catch (SqlException SQL)
+                {
+                    MessageBox.Show("Erreur dans la connexion SQL : " + SQL.Message);
+
+                }
+            }
+        }
+        
+    }
+}
